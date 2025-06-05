40c74577bcc645d7a614ec526f36861f91cd8fa3
chonlapat323 <cschonlapat@gmail.com>
2025-06-05 16:13:34 +0700

    feat(auth): add sign-in form with material UI and shared styles

--- src/app/auth/sign-in/sign-in.ts
+++ src/app/auth/sign-in/sign-in.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,4 +7,25 @@ import { Component } from '@angular/core';
   templateUrl: './sign-in.html',
   styleUrl: './sign-in.scss',
 })
-export class SignIn {}
+export class SignIn {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onLogin(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    const { email, password } = this.loginForm.value;
+    console.log('📩 Login with:', { email, password });
+
+    // TODO: call AuthService.login(email, password) here
+  }
+}
